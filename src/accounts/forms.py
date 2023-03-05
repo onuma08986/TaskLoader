771c5cb89c5da55c065aa50eadc7b9496e1e3a07
kazuyuki.oonuma <oonuma@reisys.co.jp>
2023-03-05 23:17:11 +0900

    render settings

--- src/accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from .models import User
-from django.contrib.auth.forms import UserCreationForm
-
-
-class SignUpForm(UserCreationForm):
-
-    class Meta:
-        model = User
-        fields = ('email', 'password1', 'password2')
--- src/apis/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    # RESTAPI機能（随時追記）
-    path("twitter/", include("apis.external.twitter.urls")),
-    path("youtube/", include("apis.external.youtube.urls")),
-]
