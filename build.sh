d0ae46c561782012a1de037ca28fb86bacce46a7
kazuyuki.oonuma <oonuma@reisys.co.jp>
2023-03-06 00:11:44 +0900

    render settings

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py superuser
--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SUPERUSER_NAME = env("SUPERUSER_NAME")
+SUPERUSER_EMAIL = env("SUPERUSER_EMAIL")
+SUPERUSER_PASSWORD = env("SUPERUSER_PASSWORD")
